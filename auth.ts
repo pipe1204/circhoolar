bf6ed2021a852c4b10f1645eaa9592a7c51baac0
pipe1204 <andresgon1204@gmail.com>
2023-12-28 12:59:29 +1100

    Adding Facebook provider to login

--- auth.ts
+++ auth.ts
@@ -1,50 +1,53 @@
-
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 import { adminAuth, adminDb } from "./firebase-admin";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
+import FacebookProvider from "next-auth/providers/facebook";
 
 export const authOptions: NextAuthOptions = {
-providers: [
+  providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
     }),
     EmailProvider({
-        server: {
-          host: process.env.EMAIL_SERVER_HOST,
-          port: process.env.EMAIL_SERVER_PORT,
-          auth: {
-            user: process.env.EMAIL_SERVER_USER,
-            pass: process.env.EMAIL_SERVER_PASSWORD
-          }
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
         },
-        from: process.env.EMAIL_FROM
-      }),
-],
-callbacks: {
-    session: async({session, token}) => {
-        if(session?.user) {
-            if(token.sub) {
-                session.user.id = token.sub;
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        if (token.sub) {
+          session.user.id = token.sub;
 
-                const firebaseToken = await adminAuth.createCustomToken(token.sub);
-                session.firebaseToken = firebaseToken;
-
-            }
+          const firebaseToken = await adminAuth.createCustomToken(token.sub);
+          session.firebaseToken = firebaseToken;
         }
-        return session;
+      }
+      return session;
     },
-    jwt: async({user, token}) => {
-        if(user) {
-            token.sub = user.id;
-        }
-        return token
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.sub = user.id;
+      }
+      return token;
     },
-},
-session: {
+  },
+  session: {
     strategy: "jwt",
-},
-adapter: FirestoreAdapter(adminDb)
-} satisfies NextAuthOptions
+  },
+  adapter: FirestoreAdapter(adminDb),
+} satisfies NextAuthOptions;
