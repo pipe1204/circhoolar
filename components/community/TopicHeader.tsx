a59712696e941f0e0538157bcb908fa48d387d46
Andres <66192863+pipe1204@users.noreply.github.com>
2024-01-08 18:09:17 +1100

    Adding search question component (#76)

--- components/community/TopicHeader.tsx
+++ components/community/TopicHeader.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Card } from "../ui/Card";
+import { Separator } from "../ui/separator";
 
 const TopicHeader = () => {
   return (
     <section>
       <Card className="text-light-white p-4 rounded-lg">
         <h1 className="text-2xl font-bold">Special Needs Education</h1>
+        <Separator className="text-light-white my-2" />
         <p className="text-sm text-gray-500">
-          This is the description for the topic header{" "}
+          Resources and support for parents of children with special needs,
+          including educational accommodations and therapies.
         </p>
       </Card>
     </section>
