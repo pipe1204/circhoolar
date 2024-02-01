948aeff06945d1e3542720fed4ed6a133d255e4b
pipe1204 <andresgon1204@gmail.com>
2024-02-01 15:05:20 +1100

    Commenting deletImage api route file

--- app/api/deleteImage/route.ts
+++ app/api/deleteImage/route.ts
@@ -1,34 +1,34 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import cloudinary from "cloudinary";
-import { NextResponse } from "next/server";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import cloudinary from "cloudinary";
+// import { NextResponse } from "next/server";
 
-// Configure Cloudinary
-cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// // Configure Cloudinary
+// cloudinary.v2.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
-type Data = {
-  message?: string;
-  error?: string;
-};
+// type Data = {
+//   message?: string;
+//   error?: string;
+// };
 
-export async function POST(req: NextApiRequest, res: NextApiResponse<Data>) {
-  try {
-    const requestBody = JSON.parse(req.body);
+// export async function POST(req: NextApiRequest, res: NextApiResponse<Data>) {
+//   try {
+//     const requestBody = JSON.parse(req.body);
 
-    if (!requestBody.publicId) {
-      return res.status(400).json({ error: "Missing public ID" });
-    }
+//     if (!requestBody.publicId) {
+//       return res.status(400).json({ error: "Missing public ID" });
+//     }
 
-    const publicId = requestBody.publicId;
+//     const publicId = requestBody.publicId;
 
-    await cloudinary.v2.uploader.destroy(publicId);
+//     await cloudinary.v2.uploader.destroy(publicId);
 
-    return res.status(200).json({ message: "Image deleted successfully" });
-  } catch (error) {
-    console.error("Error deleting image:", error);
-    return res.status(500).json({ error: "Error deleting image" });
-  }
-}
+//     return res.status(200).json({ message: "Image deleted successfully" });
+//   } catch (error) {
+//     console.error("Error deleting image:", error);
+//     return res.status(500).json({ error: "Error deleting image" });
+//   }
+// }
