532455238076f358288fbf0fdb9ce8b8533f4151
Andres <66192863+pipe1204@users.noreply.github.com>
2024-02-04 23:00:09 +1100

    Adding functionality to fetch institutions from Firebase for the donation in the dialog (#137)

--- lib/converters/SchoolCode.ts
+++ lib/converters/SchoolCode.ts
@@ -1,21 +1,34 @@
 import { db } from "@/firebase";
 import { SchoolCode } from "@/types/Types";
-import { DocumentData, FirestoreDataConverter, QueryDocumentSnapshot, SnapshotOptions, collection, doc } from "firebase/firestore";
+import {
+  DocumentData,
+  FirestoreDataConverter,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
+  collection,
+  doc,
+} from "firebase/firestore";
 
 const SchoolCodeConverter: FirestoreDataConverter<SchoolCode> = {
-    toFirestore: function (code: SchoolCode): DocumentData {
-        return {...code}
-    },
-    fromFirestore: function(snapshot: QueryDocumentSnapshot, options: SnapshotOptions): SchoolCode {
-        const data = snapshot.data(options);
+  toFirestore: function (code: SchoolCode): DocumentData {
+    return { ...code };
+  },
+  fromFirestore: function (
+    snapshot: QueryDocumentSnapshot,
+    options: SnapshotOptions
+  ): SchoolCode {
+    const data = snapshot.data(options);
 
-        const code: SchoolCode = {
-            id: snapshot.id,
-            schoolCode: snapshot.id,
-            name: data.name
-        } 
-        return code;
-    }
-}
+    const code: SchoolCode = {
+      id: snapshot.id,
+      schoolCode: snapshot.id,
+      name: data.name,
+      donations: data.donations,
+    };
+    return code;
+  },
+};
 
-export const codeRef = collection(db, 'schools').withConverter(SchoolCodeConverter);
+export const codeRef = collection(db, "schools").withConverter(
+  SchoolCodeConverter
+);
