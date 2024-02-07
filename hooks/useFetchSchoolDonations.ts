8a1db14057f2a0f4f5399d185b5357c8db2e6d1b
Andres <66192863+pipe1204@users.noreply.github.com>
2024-02-07 23:08:57 +1100

    Revamping homepage and adding features section. Also fixing message page with correct school name and donations button (#143)

--- hooks/useFetchSchoolDonations.ts
+++ hooks/useFetchSchoolDonations.ts
@@ -1,13 +1,11 @@
 import { useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { codeRef } from "@/lib/converters/SchoolCode";
-import { useSchoolCodeStore } from "@/store/store";
 import { Donation } from "@/types/Types";
 
 const useFetchSchoolDonations = () => {
-  const schoolCode = useSchoolCodeStore((state) => state.schoolCode);
   const [donations, setDonations] = useState<Donation[]>([]);
-  const fetchDonations = async () => {
+  const fetchDonations = async (schoolCode: string) => {
     if (schoolCode) {
       const schoolCodeDocRef = doc(codeRef, schoolCode);
       const schoolCodeDocSnap = await getDoc(schoolCodeDocRef);
